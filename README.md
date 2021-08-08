6b8ebc9f02f53389768e1d488f411cc642295976
takashiono2 <takashiono710@gmail.com>
2021-08-08 19:13:33 +0900

    section3

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Udemy 超Vue.js 2 完全パック
-<details><summary>section3</summary>
+<details><summary>section2</summary>
   <div>
     <ul>
       <li>#27ウォッチャを使ってデータが変わった時に特定の処理をする</li>
@@ -8,21 +8,27 @@
       <li>#30クラスをデータにバインディング(紐付け)する方法その2。オブジェクトを渡し、真偽値を使って動的に適応させるクラスを切り替える</li>
       <li>#31スタイル属性を、オブジェクトを用いて動的にバインディングする</li>
       <li>#32スタイルオブジェクトをデータに書いてコードを見やすくする</li>
-      <li>#33複数のスタイルオブジェクトを配列構文を用いて適応させる</li>   
+      <li>#33複数のスタイルオブジェクトを配列構文を用いて適応させる</li>
+    </ul>
+  </div>
+</details>
+<details><summary>section3</summary>
+  <div>
+    <ul>
+      <li>#39templateタグを使用して、不必要な要素を加えずにv-ifを複数の要素に適応させる</li>
+      <li>#44オブジェクトのv-forには、第２引数と第３引数にキーとインデックスを取る</li>
+      <li>#45templateタグを使用して、不必要な要素を加えずにv-forを複数の要素に適応させる</li>
+      <li>#48key属性をつける必要性を学び、予期せぬバグを起こさないv-forを作る</li>
     </ul>
   </div>
 </details>
 <details><summary>section4</summary>
   <div>
     <ul>
-      <li></li>
-    </ul>
-  </div>
-</details>
-<details><summary>section5</summary>
-  <div>
-    <ul>
-      <li></li>
+      <li>#</li>
+      <li>#</li>
+      <li>#</li>
+      <li>#</li>
     </ul>
   </div>
 </details>
--- section3/js/main.js
+++ section3/js/main.js
@@ -1,122 +1,31 @@
 (function(){
   'use strict';
+  // #45
   var app = new Vue({
     el: '#app',
     data: {
-      styleObject:{
-        color: 'red', 
-        'background-color': 'blue'
-      },
-      baseStyles:{
-        fontSize: '60px'
+      fruits:['りんご','メロン','いちご']
+    },
+    methods:{
+      remove: function(){
+        this.fruits.shift();
       }
     }
   });
+
+  // #44
+  // var app = new Vue({
+  //   el: '#app',
+  //   data: {
+  //     fruits:['りんご','メロン','いちご']
+  //   }
+  // });
+
+  // #39
+  // var app = new Vue({
+  //   el: '#app',
+  //   data: {
+  //     ok: true
+  //   }
+  // });
 })();
-
-// #32
-//   var app = new Vue({
-//     el: '#app',
-//     data: {
-//       styleObject:{
-//         color: 'red', 
-//         'background-color': 'blue'
-//       }
-//     }
-//   });
-// })();
-// //'red''blue'のように、文字列判別のため’’をつけるのを注意
-
-// #31
-//   var app = new Vue({
-//     el: '#app',
-//     data: {
-//       textColor: 'red',
-//       bgColor: 'blue'
-//     }
-//   });
-// })();
-
-// #30
-//   var app = new Vue({
-//     el: '#app',
-//     data: {
-//       isActive: true,
-//       color: 'red',
-//       bg: 'bg-blue'
-//     },
-//     computed: {
-//       classObject: function(){
-//         return {
-//           red: this.isActive,
-//           'bg-blue':!this.isActive
-//         }
-//       }
-//     }
-//   });
-// })();
-
-// #29
-//   var app = new Vue({
-//     el: '#app',
-//     data: {
-//       isActive: true
-//     },
-//     computed: {
-//       classObject: function(){
-//         return {
-//           red: this.isActive,
-//           'bg-blue':!this.isActive
-//         }
-//       }
-//     }
-//   });
-// })();
-
-//#28
-//   var app = new Vue({
-//     el: '#app',
-//     data: {
-//       counter: 0
-//     },
-//     computed:{
-//       doubleCouneterComputed: function(){
-//         return this.counter * 2;
-//       }
-//     },
-//     methods:{
-//       countUp: function(){
-//         this.counter += 1;
-//       },
-//       doubleCouneterMethod:function(){
-//         return this.counter * 2;
-//       }
-//     }
-//   });
-// })();
-
-//#27
-// (function(){
-//   'use strict';
-//   var app = new Vue({
-//     el: '#app',
-//     data:{
-//       counter: 0
-//     },
-//     computed:{
-//       lessThanThree: function(){
-//         return this.counter > 3 ? '3より上' :'3より下';
-//       }
-//     },
-//     watch: {//coumputed処理しつつ、非同期処理が働く
-//       counter: function(){
-//         var vm = this;  
-//         setTimeout(function(){
-//             vm.counter = 0;
-//         },3000);
-//       }
-//     }
-//   });
-// })();
-// computedとwatchは似ている。が、computedでは、処理しきれないことがある。watchは特定の処理をする時に使う。
-// watchは「非同期処理」の時に使う。また、「非同期処理」の時は、returnが使えない。
