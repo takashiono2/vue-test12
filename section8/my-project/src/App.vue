c7b67b7b7eb692f0aea6d97b7f278d3bad30f45a
takashiono2 <takashiono710@gmail.com>
2021-08-10 18:54:30 +0900

    section8

--- section8/my-project/src/App.vue
+++ section8/my-project/src/App.vue
@@ -1,17 +1,66 @@
 <template>
-  <div><!-- v-solt:（引数名）でslotを指定できる。v-soltを使う時は、templateで区切る-->
+  <div>
     <LikeHeader>
-      <template v-slot:title>
+      <h3>はじめまして</h3>
+    </LikeHeader>
+    <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
+    <button @click="currentComponent = 'Home'">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <keep-alive>
+      <component :is="currentComponent"></component>
+    </keep-alive>
+  </div>
+</template>
+<!--componentは毎回destroyされる（毎回インスタンスが生成される）-->
+<script>
+import LikeHeader from './components/LikeHeader.vue';
+import About from './components/About.vue';
+import Home from './components/Home.vue';
+
+export default {
+  data(){
+    return {
+      number: 14,
+      currentComponent: "Home"
+    };
+  },
+  components:{
+    LikeHeader,
+    About,
+    Home
+  },
+  methods:{
+    incrementNumber(value){
+      this.number = value;
+    }
+  }
+};
+</script>
+
+<style scoped>
+div{
+  border: 1px solid blue;
+}
+</style>
+
+<!--#108
+<template>
+  <div>--><!-- v-solt:（引数名）でslotを指定できる。v-soltを使う時は、templateで区切る-->
+    <!--<LikeHeader>-->
+      <!-- <template v-slot:title="slotProps">のように、v-soltに命名すると、子からデータを引っ張れる -->
+      <!--<template v-slot:title="slotProps">
         <h2>こんにちは</h2>
+        <h2>{{ slotProps.user }}</h2>
+        <h2>{{ slotProps.text }}</h2>
       </template>
       <template v-slot:number> 
         <p>{{number}}</p>
       </template> 
     </LikeHeader>
-    <LikeNumber :total-number="number" @my-click="number=$event"></LikeNumber>
+    <LikeNumber :total-number="number" @my-click="number=$event"></LikeNumber>-->
     <!-- <LikeNumber :totalNumber="number"></LikeNumber> -->
-    <LikeNumber :total-number="number"></LikeNumber>
-  </div>
+    <!-- <LikeNumber :total-number="number"></LikeNumber>-->
+  <!--</div>
 </template>
 
 <script>
@@ -38,6 +87,6 @@ h1{
   color:red;
 } */
 
-</style>
+</style>-->
 <!--slotの時、sytleは親、子どちらで指定してもいいが、sytle以外は書いた方に適用される-->
 <!--sytleは親、子両方に記述した場合は、親が優先される-->
--- section8/my-project/src/components/LikeHeader.vue
+++ section8/my-project/src/components/LikeHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <div><!-- v-slot:（名前）の名前の部分をname属性で指定する-->
-    <slot name="title"></slot>
+  <div>
+    <slot name="title" :user="user" text="text"></slot>
     <hr>
     <p>いいねの数</p>
     <slot name="number"></slot>
@@ -9,7 +9,14 @@
 
 <script>
 export default{
-  props:["headerText"]
+  data: function(){
+    return {
+      user:{
+        firstName: "Jack",
+        lastname: "Donald"
+      }
+    }
+  }
 };
 </script>
 
@@ -18,3 +25,33 @@ h1{
   color:red;
 }
 </style>
+
+<!--#108--><!-- v-slot:（名前）の名前の部分をname属性で指定する-->
+<!--<template>-->
+  <!--<div>-->
+    <!--<slot name="title" :user="user" text="text"></slot>--><!-- dataにあるuserをv-bindするとデータ子から親へ送れる-->
+    <!--<hr>-->
+    <!--<p>いいねの数</p>-->
+    <!--<slot name="number"></slot>
+  </div>
+</template>-->
+<!--
+<script>
+export default{
+  data: function(){
+    return {
+      user:{
+        firstName: "Jack",
+        lastname: "Donald"
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+h1{
+  color:red;
+}
+</style>
+-->
