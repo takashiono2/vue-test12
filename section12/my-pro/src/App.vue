1412edb0b88fa1c31810a5802679ee2be5089014
takashiono2 <takashiono710@gmail.com>
2021-08-13 23:23:33 +0900

    section12

--- section12/my-pro/src/App.vue
+++ section12/my-pro/src/App.vue
@@ -3,7 +3,39 @@
     <button @click="myAnimation = 'slide'">Slide</button>
     <button @click="myAnimation = 'fade'">Fade</button>
     <p>{{ myAnimation }}</p>
-    <button @click="show=!show">切り替え</button>
+    <br>
+    <button @click="add">追加</button>
+    <ul style="width: 200px; margin: auto;">
+      <transition-group name="fade">
+        <li 
+          style="cursor: pointer;"
+          v-for="(number,index) in numbers"
+          @click="remove(index)"
+          :key="number"
+        >
+        {{number}}</li>
+      </transition-group>
+    </ul>
+    <br>
+    <button @click="show =!show">切り替え</button>
+    <br><br>
+    <transition
+      :css="false"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @leave="leave"
+    >
+      <div 
+        class="circle" 
+        v-if="show"
+      ></div>
+    </transition>
+    <br>
+    <button @click="myComponent ='ComponentA'">ComponentA</button>
+    <button @click="myComponent ='ComponentB'">ComponentB</button>
+    <transition name="fade" mode="out-in">
+      <component :is="myComponent"></component>
+    </transition>
     <transition 
       name="fade" 
       mode="out-in"
@@ -29,31 +61,90 @@
 </template>
 
 <script>
+  import ComponentA from './components/ComponentA.vue';
+  import ComponentB from './components/ComponentB.vue';
   export default {
+    components:{
+      ComponentA,
+      ComponentB
+    },
     data: function(){
       return {
+        numbers:[0,1,2],
+        nextNumber: 3,
         show: true,
-        myAnimation :'slide'
+        myAnimation :'slide',
+        myComponent :'ComponentA'
       };
+    },
+    methods: {
+      randomIndex(){//0,1,2をランダムに返したい
+        return Math.floor(Math.random() * this.numbers.length);
+      },
+      add(){
+        this.numbers.splice(this.randomIndex(),0,this.nextNumber);
+        this.nextNumber +=1;
+      },
+      remove(index){
+        this.numbers.splice(index,1);
+      },
+      beforeEnter(el){
+        el.style.transform = `scale(0)`;
+      },
+      enter(el,done){
+        let scale = 0;
+        const interval = setInterval(function(){
+          el.style.transform = `scale(${scale})`;
+          scale += 0.1; 
+          if(scale > 1){
+            clearInterval(interval);
+            done();
+          }
+        },200);
+      },
+      leave(el,done){
+        let scale = 1;
+        const interval = setInterval(function(){
+          el.style.transform = `scale(${scale})`;
+          scale -= 0.1; 
+          if(scale < 0){
+            clearInterval(interval);
+            done();
+          }
+        },200);
+      }
     }
   };
 </script>
 
 <style scoped>
+.circle{
+  width: 200px;
+  height: 200px;
+  margin: auto;
+  border-radius: 100px;
+  background-color: pink;
+}
+.fade-move{
+  transition: transform 1s;
+}
+
 .fade-enter{
   opacity: 0;
 }
 .fade-enter-active{
-  transition: opacity 0.5s;
+  transition: opacity 1s;
 }
 .fade-enter-to{
   opacity: 1;
 }
 .fade-leave{
   opacity: 1;
+  position: absolute;
+  width: 200px;
 }
 .fade-leave-active{
-  transition: opacity 0.5s;
+  transition: opacity 1s;
 }
 .fade-leave-to{
   opacity: 0;
