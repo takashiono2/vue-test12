b609aaf41a2bd2445b6cc84e0faae40fb4c58348
takashiono2 <takashiono710@gmail.com>
2021-08-28 19:02:54 +0900

    section18の252

--- section18/my-pro/src/store/index.js
+++ section18/my-pro/src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from '../axios-auth';
 import router from '../router';
+import axiosRefresh from '../axios-refresh'
 
 Vue.use(Vuex);
 
@@ -18,7 +19,7 @@ export default new Vuex.Store({
     }
   },
   actions:{
-    login({commit},authData){
+    login({commit,dispatch},authData){
       axios
       .post(
         '/accounts:signInWithPassword?key=AIzaSyD_MIW0u5iXeAzxB5d1QJPQqLcio8gPhJs',
@@ -29,7 +30,25 @@ export default new Vuex.Store({
       }
     ).then(response =>{
       commit('updateIdToken',response.data.idToken);
+      setTimeout(()=>{
+        dispatch('refreshIdToken',response.data.idToken);
+      },
+      response.data.expiresIn * 1000);
       router.push('/');
+    });
+  },
+  refreshIdToken({commit,dipatch},refreshToken){
+    axiosRefresh
+      .post('/token?key=AIzaSyD_MIW0u5iXeAzxB5d1QJPQqLcio8gPhJs',
+    {
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken
+    })
+    .then(response=>{
+      commit('updateIdToken',response.data.id_token);
+      setTimeout(()=>{
+        dipatch('refreshIdToken',response.data.refresh_token);
+      },response.data.expires_in * 1000);
     });
   },
     register({commit},authData){
